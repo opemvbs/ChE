03c4459de15d2c02ec132adc22f58e257a324da4
opemvbs <62207297+opemvbs@users.noreply.github.com>
2020-06-29 03:24:33 +0700

    Create 2.1.py

---
+++ pycse/2 Basic python usage/2.1 Basic math/2.1.py
@@ -0,0 +1,19 @@
+# addition
+print(2 + 4) 
+# subtraction
+print(8.1 - 5) 
+# multiplication
+print(5 * 4) 
+print(3.1 * 2)
+# division float type
+print(4.0 / 2.0) 
+print(1.0 / 3.1)
+print(4 // 2)
+print(1 // 3)
+# division integer type
+print(4 / 2) 
+print(1 / 3)
+# exponentiation
+print(3.0 ** 2) 
+print(3 ** 2)
+print(2 ** 0.5)
